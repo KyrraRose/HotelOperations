63639bd9333b1899eea0f2aa219b49756979ec90
KyrraRose <krbosland1@gmail.com>
2025-10-20 14:03:38 -0400

    Made room and reservation classes

---
+++ src/main/java/com/pluralsight/Room.java
@@ -0,0 +1,48 @@
+package com.pluralsight;
+
+public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean occupied,dirty,available;
+
+    public Room(int numberOfBeds, double price, boolean occupied, boolean dirty, boolean available) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.occupied = occupied;
+        this.dirty = dirty;
+        this.available = available;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
+
+    public boolean isAvailable() {
+        return (!dirty&&!occupied);
+    }
+
+}
